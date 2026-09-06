f4b80a5645a64bb5655b7f1e3861edc0230a8cb0
rnldy <renaldycahyaa@gmail.com>
2026-09-06 14:14:02 +0700

    fix(brand): support dark mode logo

--- src/components/brand/brand-mark.tsx
+++ src/components/brand/brand-mark.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image"
 
 export function BrandMark({ className = "" }: { className?: string }) {
-  return <Image src="/img/yapping-saham-logo/logo.svg" alt="Yapping Saham" width={240} height={60} priority className={className} />
+  return <>
+    <Image src="/img/yapping-saham-logo/logo.svg" alt="Yapping Saham" width={240} height={60} priority className={`dark:hidden ${className}`} />
+    <Image src="/img/yapping-saham-logo/logo-dark.svg" alt="Yapping Saham" width={240} height={60} priority className={`hidden dark:block ${className}`} />
+  </>
 }
